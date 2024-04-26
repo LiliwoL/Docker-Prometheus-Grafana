e15ae4e5dc736ed122c731df5587920b82708047
Nicolas C. <github@liliwol.fr>
2024-04-26 17:31:12 +0200

    First

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Configurer les identifiants de grafana dans .env.docker
+`GF_SECURITY_ADMIN_USER=admin
+GF_SECURITY_ADMIN_PASSWORD=admin`
+
+
+Grafana:
+http://localhost:3000
+
+Prometheus:
+http://localhost:9090
+
+![](readme_docs/57eb2a31.png)
+
+Apache
+http://localhost:8080
+
+
+# Surveillez Apache avec Prometheus et Grafana
+https://fr.linux-console.net/?p=21996
---
+++ config/apache/apache_exporter_installer.sh
@@ -0,0 +1,23 @@
+# Installation des paquets nécessaires
+apt update && apt install wget curl
+
+# Récupération de la dernière version du apache exporter
+curl -s https://api.github.com/repos/Lusitaniae/apache_exporter/releases/latest|grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+
+# Décompression
+tar xvf apache_exporter-*.linux-amd64.tar.gz
+
+# Copie du binaire dans le dossier /usr/local/bin
+cp apache_exporter-*.linux-amd64/apache_exporter /usr/local/bin
+
+chmod +x /usr/local/bin/apache_exporter
+
+# Création de l'utilisateur et du groupe prometheus
+groupadd --system prometheus
+useradd -s /sbin/nologin --system -g prometheus prometheus
+
+# Lancement de apache_exporter
+apache_exporter &
+
+# Vérification
+netstat -plnt | grep 9117
